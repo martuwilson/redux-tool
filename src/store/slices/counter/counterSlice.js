1570c47ae7bd69011ec1914f1432667c1164975c
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-06-17 12:42:17 -0300

    config store and add counterSlice

---
+++ src/store/slices/counter/counterSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+      counter: 10,
+      times: 0
+  },
+  reducers: {
+    increment: (state) => {
+      state.counter += 1;
+    },
+    incrementBy: ( state, action ) => {
+      console.log(action);
+      state.counter += action.payload;
+    },
+    decrement: ( state ) => {
+      state.counter -= 1;
+    },
+    
+  },
+})
+
+export const { increment, decrement, incrementBy } = counterSlice.actions;
